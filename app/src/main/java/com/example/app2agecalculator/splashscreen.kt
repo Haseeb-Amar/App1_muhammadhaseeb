205f65cd2bb7bc7645fa1f3725f82682b40cbd9a
Haseeb-Amar <haseeebamar55@gmail.com>
2022-12-11 20:04:24 +0400

    age calculator

---
+++ app/src/main/java/com/example/app2agecalculator/splashscreen.kt
@@ -0,0 +1,25 @@
+package com.example.app2agecalculator
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
+import kotlinx.coroutines.delay
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intentt = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+    }
+}
